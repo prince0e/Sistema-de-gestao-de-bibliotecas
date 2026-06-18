7e92c6651bcc97b94fdab7029cbec20a9d857206
Prince <princedejesus266@gmail.com>
2026-06-18 21:35:53 +0200

    Inicializar o código

--- index.c
+++ index.c
@@ -1,1 +1,8 @@
+#include <stdio.h> //biblioteca para entrada e saida de dados
+#include <stdlib.h> //biblioteca para chamadas no sistema e gerenciamento de memória
 
+int main() {
+  printf("\n\nObrigado por usar o nosso sistema de gestao de bibliotecas!");
+
+  return 0;
+}
