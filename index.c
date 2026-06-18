02179104f3baa8c1de3ceb8bc0e8ca06825662ca
Prince <princedejesus266@gmail.com>
2026-06-18 22:46:00 +0200

    Criar o menu inicial

--- index.c
+++ index.c
@@ -1,8 +1,24 @@
 #include <stdio.h> //biblioteca para entrada e saida de dados
 #include <stdlib.h> //biblioteca para chamadas no sistema e gerenciamento de memória
 
+void menuInicial() {
+  printf("\
+╔════════════════════════════════════════════════╗\n\
+║ Bem vindo ao sistema de gestão de bibliotecas  ║\n\
+╠════════════════════════════════════════════════╣\n\
+║ 1. Entrar como administrador                   ║\n\
+║ 2. Entrar como bibliotecário                   ║\n\
+║ 3. Entrar como leitor                          ║\n\
+╚════════════════════════════════════════════════╝\
+  ");
+
+}
+
 int main() {
-  printf("\n\nObrigado por usar o nosso sistema de gestao de bibliotecas!");
+  menuInicial();
+
+
+  printf("\n\nObrigado por usar sistema de gestao de bibliotecas!");
 
   return 0;
 }
